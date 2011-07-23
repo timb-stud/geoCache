7dc5d5186573430e3c3d9a8e894b3edc28f34292
Tim Bartsch <timb.stud@gmail.com>
2011-07-23 15:04:37 +0200

    moved map.init()to  geoCache.initMap()

--- geoCache.js
+++ geoCache.js
@@ -18,6 +18,15 @@ var geoCache = {
 			});
 		}
 	},
+	initMap: function(){
+		var latlng = new google.maps.LatLng(49.000000, 6.000000),
+			options = {
+				zoom: 8,
+				center: latlng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+		var map = new google.maps.Map(document.getElementById("map-canvas"), options);
+	},
 	supports_geolocation: function(){
 		return !!navigator.geolocation;
 	}
