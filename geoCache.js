827d6addead5c199cb1b68f8cae9a6f9dafbda73
Tim Bartsch <timb.stud@gmail.com>
2011-07-23 16:09:19 +0200

    added marker.

--- geoCache.js
+++ geoCache.js
@@ -19,16 +19,57 @@ var geoCache = {
 		}
 	},
 	initMap: function(){
-		console.log("map init");
-		var latlng = new google.maps.LatLng(49.000000, 6.000000),
-			options = {
-				zoom: 8,
-				center: latlng,
+		var urlVars = geoCache.getUrlVars();
+		console.log(urlVars);
+		var centerLatlng;
+		if(urlVars["lat"] && urlVars["lon"]){
+			centerLatlng = new google.maps.LatLng(urlVars["lat"], urlVars["lon"]);
+		}else{
+			centerLatlng = new google.maps.LatLng(49, 6);
+		}
+		var options = {
+				zoom: 15,
+				center: centerLatlng,
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
 		var map = new google.maps.Map(document.getElementById("map-canvas"), options);
+		if(urlVars["lat"] && urlVars["lon"]) {
+			var currentLatlng = new google.maps.LatLng(urlVars["lat"], urlVars["lon"]);
+			var currentMarker = new google.maps.Marker({
+				position : currentLatlng,
+				title : "Your Position",
+				map: map
+			});
+			var currentInfoWindow = new google.maps.InfoWindow({
+				content: "Your Position",
+			});
+			currentInfoWindow.open(map, currentMarker);
+		}
+		if(urlVars["latDest"] && urlVars["lonDest"]) {
+			var destLatlng = new google.maps.LatLng(urlVars["latDest"], urlVars["lonDest"]);
+			var destMarker = new google.maps.Marker({
+				position : destLatlng,
+				title : "Destination",
+				map: map
+			});
+			var destInfoWindow = new google.maps.InfoWindow({
+				content: "Destination",
+			});
+			destInfoWindow.open(map, destMarker);
+		}
 	},
 	supports_geolocation: function(){
 		return !!navigator.geolocation;
+	},
+	getUrlVars: function(){
+		var vars = [], hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		for(var i = 0; i < hashes.length; i++)
+		{
+		    hash = hashes[i].split('=');
+		    vars.push(hash[0]);
+		    vars[hash[0]] = hash[1];
+		}
+		return vars;
 	}
 };
