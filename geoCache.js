36c022203e4bcd5902e91926ead0013f35ed676a
Tim Bartsch <timb.stud@gmail.com>
2011-07-23 18:45:25 +0200

    removed address.

--- geoCache.js
+++ geoCache.js
@@ -43,18 +43,14 @@ var geoCache = {
 	getPosition: function(){
 		if(geoCache.supports_geolocation()){
 			navigator.geolocation.getCurrentPosition(function(pos){
-				var city = pos.address.city,
-					street = pos.address.street,
-					streetNumber = pos.address.streetNumber,
-					description = pos.classDescription,
-					accuracy = pos.coords.accuracy,
+				console.log(pos);
+				var accuracy = pos.coords.accuracy,
 					lat = pos.coords.latitude,
 					lon = pos.coords.longitude;
 				console.log(city, street, streetNumber);
 				$("span#current-lat").text(lat);
 				$("span#current-lon").text(lon);
 				$("span#current-accuracy").text(accuracy);
-				$("span#current-address").text(city + " " + street + " " + streetNumber);
 			}, function(){
 				alert("ERROR: Cant't get your location.");
 			}, {enableHighAccuracy: true});
